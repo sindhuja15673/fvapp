4ea70e19b379f84a85aea3072f3c5f991fff0bb0
something-man <minnalkodi735@gmail.com>
2025-01-02 15:37:33 +0530

    firsts

--- my-backend/server.js
+++ my-backend/server.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
 // });
-const HOST = '0.0.0.0'; // Bind to all network interfaces
+const HOST = 'localhost'; // Bind to all network interfaces
 
 app.listen(PORT, HOST, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
